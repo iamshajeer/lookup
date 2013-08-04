89a0faaeeb31f44ff459bfcd58a42346d5c24b2e
Alexey Kuznetsov <axet@me.com>
2013-08-04 16:59:03 +0400

    separate test from sarray

--- src/main/java/com/github/axet/lookup/common/SArray.java
+++ src/main/java/com/github/axet/lookup/common/SArray.java
@@ -11,7 +11,7 @@ public class SArray {
     public int cy;
 
     public double s[];
-    
+
     public SArray() {
 
     }
@@ -100,31 +100,4 @@ public class SArray {
         Capture.writeDesktop(getImage());
     }
 
-    public static void main(String[] args) {
-        int[] s = new int[] {
-
-        5, 2, 5, 2,
-
-        3, 6, 3, 6,
-
-        5, 2, 5, 2,
-
-        3, 6, 3, 6
-
-        };
-
-        SArray a = new SArray();
-        a.cx = 4;
-        a.cy = 4;
-        a.s = new double[a.cx * a.cy];
-
-        for (int y = 0; y < a.cy; y++) {
-            for (int x = 0; x < a.cx; x++) {
-                a.s[a.i(x, y)] = s[a.i(x, y)] + a.s(x - 1, y) + a.s(x, y - 1) - a.s(x - 1, y - 1);
-            }
-        }
-
-        boolean t = a.sigma() == 64;
-
-    }
 }
---
+++ src/test/java/com/github/axet/lookup/SArrayTest.java
@@ -0,0 +1,34 @@
+package com.github.axet.lookup;
+
+import com.github.axet.lookup.common.SArray;
+
+public class SArrayTest {
+
+    public static void main(String[] args) {
+        int[] s = new int[] {
+
+        5, 2, 5, 2,
+
+        3, 6, 3, 6,
+
+        5, 2, 5, 2,
+
+        3, 6, 3, 6
+
+        };
+
+        SArray a = new SArray();
+        a.cx = 4;
+        a.cy = 4;
+        a.s = new double[a.cx * a.cy];
+
+        for (int y = 0; y < a.cy; y++) {
+            for (int x = 0; x < a.cx; x++) {
+                a.s[a.i(x, y)] = s[a.i(x, y)] + a.s(x - 1, y) + a.s(x, y - 1) - a.s(x - 1, y - 1);
+            }
+        }
+
+        boolean t = a.sigma() == 64;
+
+    }
+}
